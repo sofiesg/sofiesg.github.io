23357558b508fa0dce1d9b5ec59aec729324b467
sofiesg <sofiesg@stud.ntnu.no>
2024-09-17 15:27:16 +1000

    Added video in HTML

--- week7/script.js
+++ week7/script.js
@@ -8,3 +8,20 @@ playButton.addEventListener("click", playAudio);
 function playAudio() {
   notify.play();
 }
+
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
+pauseButton.addEventListener("click", pauseAudio);
+function pauseAudio() {
+  notify.pause();
+}
+
+const popSound = document.querySelector("#pop-sound");
+console.log(popSound);
+
+const popButton = document.querySelector("#pop-button");
+console.log(popButton);
+popButton.addEventListener("click", popAudio);
+function popAudio() {
+  popSound.play();
+}
