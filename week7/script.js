201e1c57c2c32c447f5d5abd33fe89c56b75567c
sofiesg <sofiesg@stud.ntnu.no>
2024-09-17 15:31:33 +1000

    myVideo again??

--- week7/script.js
+++ week7/script.js
@@ -9,14 +9,14 @@ const playButton = document.querySelector("#play-button");
 console.log(playButton);
 playButton.addEventListener("click", playAudio);
 function playAudio() {
-  notify.play();
+  myVideo.play();
 }
 
 const pauseButton = document.querySelector("#pause-button");
 console.log(pauseButton);
 pauseButton.addEventListener("click", pauseAudio);
 function pauseAudio() {
-  notify.pause();
+  myVideo.pause();
 }
 
 const popSound = document.querySelector("#pop-sound");
@@ -28,3 +28,27 @@ popButton.addEventListener("click", popAudio);
 function popAudio() {
   popSound.play();
 }
+
+// const playButton = document.querySelector("#play-button");
+// console.log(playButton);
+// playButton.addEventListener("click", playAudio);
+// function playAudio() {
+//   notify.play();
+// }
+
+// const pauseButton = document.querySelector("#pause-button");
+// console.log(pauseButton);
+// pauseButton.addEventListener("click", pauseAudio);
+// function pauseAudio() {
+//   notify.pause();
+// }
+
+// const popSound = document.querySelector("#pop-sound");
+// console.log(popSound);
+
+// const popButton = document.querySelector("#pop-button");
+// console.log(popButton);
+// popButton.addEventListener("click", popAudio);
+// function popAudio() {
+//   popSound.play();
+// }
