a433aa915b294608756f0bfb49aa962a1b1f89a3
sofiesg <sofiesg@stud.ntnu.no>
2024-09-17 16:18:49 +1000

    Play pause button
    Added Play pause image changes to the button!

---
+++ week7/videoplayer/script.js
@@ -0,0 +1,19 @@
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+
+const playPauseButton = document.querySelector("#play-pause-button");
+console.log(playPauseButton);
+
+playPauseButton.addEventListener("click", playPauseVideo);
+
+const playPauseImg = document.querySelector("#play-pause-img");
+
+function playPauseVideo() {
+  if (myVideo.paused || myVideo.ended) {
+    myVideo.play();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v2.png";
+  } else {
+    myVideo.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
+  }
+}
