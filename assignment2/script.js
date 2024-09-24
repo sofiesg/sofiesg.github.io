c7b4a9d2eefe483eddbff41defe3e519ff57316e
sofiesg <sofiesg@stud.ntnu.no>
2024-09-24 13:52:32 +1000

    Fixed dark mode (mostly - prog) start week8

--- assignment2/script.js
+++ assignment2/script.js
@@ -2,3 +2,4 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+// added dark mode to meet users' different needs and make the site more comfortable for late night use when studying
