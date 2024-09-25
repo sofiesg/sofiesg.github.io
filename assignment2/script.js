48458a00d8f2e46b3f3ca914a0c29a3c92a8f32e
sofiesg <sofiesg@stud.ntnu.no>
2024-09-26 00:50:56 +1000

    Font change-removed vids - bring back controls + Java
    RIGHT before the buttons changed (has to do with css and the video-control changes.

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,3 +1,6 @@
+const myMusic = document.querySelector("#my-music");
+console.log(myMusic);
+
 function colorMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
@@ -11,3 +14,22 @@ function colorMode() {
 // added dark mode to meet users' different needs and make the site more
 // comfortable for late night use when studying. I have not made the dark mode
 // standard as a light background is usually better for readability.
+
+const muteBtn = document.querySelector("#mute-btn");
+console.log(muteBtn);
+
+muteBtn.addEventListener("click", toggleSound);
+
+const muteImg = document.querySelector("#mute-img");
+
+function toggleSound() {
+  if (myMusic.muted) {
+    myMusic.muted = false;
+    muteUnmuteBtn.style.backgroundColor = "d5cea3";
+    muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+  } else {
+    myMusic.muted = true;
+    muteBtn.style.backgroundColor = "#8c886f";
+    muteImg.src = "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
+  }
+}
