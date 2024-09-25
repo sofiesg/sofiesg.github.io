e23d1fcb14c7a7e12e5251b92b707aa73652c772
sofiesg <sofiesg@stud.ntnu.no>
2024-09-26 00:59:18 +1000

    Buttons function now (needed ID)
    Starting to have a functional music player :) playlist next!

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,6 +1,35 @@
 const myMusic = document.querySelector("#my-music");
 console.log(myMusic);
 
+const playButton = document.querySelector("#play-btn");
+console.log(playButton);
+
+playButton.addEventListener("click", togglePlay);
+
+const playImg = document.querySelector("#play-img");
+
+function togglePlay() {
+  if (myMusic.paused || myMusic.ended) {
+    myMusic.play();
+    playImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+  } else {
+    myMusic.pause();
+    playImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
+  }
+}
+
+function toggleSound() {
+  if (myMusic.muted) {
+    myMusic.muted = false;
+    muteUnmuteBtn.style.backgroundColor = "d5cea3";
+    muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+  } else {
+    myMusic.muted = true;
+    muteBtn.style.backgroundColor = "#8c886f";
+    muteImg.src = "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
+  }
+}
+
 function colorMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
@@ -21,15 +50,3 @@ console.log(muteBtn);
 muteBtn.addEventListener("click", toggleSound);
 
 const muteImg = document.querySelector("#mute-img");
-
-function toggleSound() {
-  if (myMusic.muted) {
-    myMusic.muted = false;
-    muteUnmuteBtn.style.backgroundColor = "d5cea3";
-    muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
-  } else {
-    myMusic.muted = true;
-    muteBtn.style.backgroundColor = "#8c886f";
-    muteImg.src = "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
-  }
-}
