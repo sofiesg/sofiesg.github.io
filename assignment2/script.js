2dd9b6a0f97631bd79308723307cb32ab6929b9d
sofiesg <sofiesg@stud.ntnu.no>
2024-09-25 15:20:27 +1000

    Fixed the nav + button center issues

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,7 +1,13 @@
-function myFunction() {
+function colorMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+  var x = document.getElementById("mode-text");
+  if (x.innerHTML === "Light mode") {
+    x.innerHTML = "Dark mode";
+  } else {
+    x.innerHTML = "Light mode";
+  }
 }
 // added dark mode to meet users' different needs and make the site more
 // comfortable for late night use when studying. I have not made the dark mode
-// standard as a light background is usually better for readability
+// standard as a light background is usually better for readability.
