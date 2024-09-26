44d209b83923309ec664f52d363356e1e572f972
sofiesg <sofiesg@stud.ntnu.no>
2024-09-26 23:17:21 +1000

    who f knowsa

--- assignment2/script.js
+++ assignment2/script.js
@@ -18,17 +18,70 @@ function togglePlay() {
   }
 }
 
+const muteBtn = document.querySelector("#mute-btn");
+console.log(muteBtn);
+
+muteBtn.addEventListener("click", toggleSound);
+
+const muteImg = document.querySelector("#mute-img");
+
 function toggleSound() {
   if (myMusic.muted) {
     myMusic.muted = false;
-    muteUnmuteBtn.style.backgroundColor = "d5cea3";
-    muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+    muteBtn.style.backgroundColor = "d5cea3";
+    muteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
   } else {
     myMusic.muted = true;
     muteBtn.style.backgroundColor = "#8c886f";
     muteImg.src = "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
   }
 }
+
+myMusic.addEventListener("timeupdate", showProgress);
+
+const progressBar = document.querySelector("#progress-bar-fill");
+const songTime = document.querySelector("#music-time");
+
+function showProgress() {
+  const currentTime = myMusic.currentTime;
+  //   console.log("current time", currentTime.toFixed(2));
+  songTime.textContent = currentTime.toFixed(1);
+  const progress = (currentTime / myMusic.duration) * 100;
+  console.log("progress", progress.toFixed(2));
+  //   progressBar.style.width = progress + "%";
+}
+
+// const myMusic = document.querySelector("#my-music");
+// console.log(myMusic);
+
+// const playButton = document.querySelector("#play-btn");
+// console.log(playButton);
+
+// playButton.addEventListener("click", togglePlay);
+
+// const playImg = document.querySelector("#play-img");
+
+// function togglePlay() {
+//   if (myMusic.paused || myMusic.ended) {
+//     myMusic.play();
+//     playImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+//   } else {
+//     myMusic.pause();
+//     playImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
+//   }
+// }
+
+// function toggleSound() {
+//     if (myMusic.muted) {
+//       myMusic.muted = false;
+//       muteUnmuteBtn.style.backgroundColor = "d5cea3";
+//       muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+//     } else {
+//       myMusic.muted = true;
+//       muteBtn.style.backgroundColor = "#8c886f";
+//       muteImg.src = "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
+//     }
+//   }
 
 function colorMode() {
   var element = document.body;
@@ -43,10 +96,3 @@ function colorMode() {
 // added dark mode to meet users' different needs and make the site more
 // comfortable for late night use when studying. I have not made the dark mode
 // standard as a light background is usually better for readability.
-
-const muteBtn = document.querySelector("#mute-btn");
-console.log(muteBtn);
-
-muteBtn.addEventListener("click", toggleSound);
-
-const muteImg = document.querySelector("#mute-img");
