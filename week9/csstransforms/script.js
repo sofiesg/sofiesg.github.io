104d64f98b83b23158adb8e3047d90c73ce01808
sofiesg <sofiesg@stud.ntnu.no>
2024-10-01 15:18:07 +1000

    prettier works baby!!

--- week9/csstransforms/script.js
+++ week9/csstransforms/script.js
@@ -5,12 +5,75 @@ console.log(ball)
 
 // -------------------------------------------
 
+// Let us access the outer box and check it's width
+const outer = document.querySelector(".outer");
+console.log(outer);
+let outerWidth = outer.clientWidth / 2 -60;
+console.log(outerWidth);
+
 // let us work on the move or translate logic
 const moveButton = document.querySelector("#move-button")
 console.log(moveButton)
 moveButton.addEventListener("click", moveBall);
 
-function moveBall()
-{
-    ball.style.transform = `translateX(50px)`;
+let distance = 0;
+function moveBall() {
+    if (distance < outerWidth +30) {
+        distance += 30;
+        console.log(distance);
+        transformBall();
+        // ball.style.transform = `translateX(${distance}px)`
+    }
+    // distance += 30;
+    // console.log(distance);
+    // ball.style.transform = `translateX(${distance}px)`;
+    // ball.style.transform = `translateX(${distance}px) translateY(${distance}px)`;
+}
+
+// let us work on the rotate logic
+const rotateButton = document.querySelector("#rotate-button");
+console.log(rotateButton);
+rotateButton.addEventListener("click", rotateBall);
+
+let angle = 0;
+function rotateBall() {
+    angle += 45;
+    transformBall();
+    // ball.style.transform = `rotate(${angle}deg)`
+}
+
+//---------------------------------------------------
+
+// let us work on the scale logic
+
+const scaleButton = document.querySelector("#scale-button");
+console.log(scaleButton);
+scaleButton.addEventListener("click", scaleBall);
+
+let point = 1;
+function scaleBall() {
+    if (point > 0.22) {
+        point -= 0.1;
+        console.log(point);
+        transformBall();
+        // ball.style.transform = `scale(${point})`
+    }
+    // point -= 0.1;
+    // ball.style.transform = `scale(${point})`
+}
+
+// le tus create an universal transform function
+function transformBall() {
+    ball.style.transform = `scale(${point}) rotate(${angle}deg) translateX(${distance}px)`;
+}
+
+// let us work on the reset logic
+const resetButton = document.querySelector("#reset-button");
+console.log(resetButton);
+resetButton.addEventListener("click", resetBall);
+function resetBall() {
+    distance = 0;
+    angle = 0;
+    point = 1;
+    transformBall();
 }
