5bb5523e715d5229a6abc867b1fae4dc963c9aab
sofiesg <sofiesg@stud.ntnu.no>
2024-10-08 15:55:54 +1100

    everything + slides

---
+++ week10/script.js
@@ -0,0 +1,124 @@
+// Let us first work on the hover effect
+const myCat = document.querySelector("#my-cat");
+console.log(myCat);
+const moreInfo = document.querySelector("#more-info");
+console.log(moreInfo);
+myCat.addEventListener("mouseover", showMoreInfo);
+myCat.addEventListener("mouseout", hideMoreInfo);
+
+//adding show class containing display:block will make the div visible
+function showMoreInfo() {
+  console.log("mouse is inside the image");
+  moreInfo.classList.add("show");
+}
+
+//taking the show less out will make the div revert back to display:none
+function hideMoreInfo() {
+  moreInfo.classList.remove("show");
+}
+
+//---------------------------------------------
+
+// let us work on the drop down example
+
+const profileButton = document.querySelector("#profile-button");
+console.log(profileButton);
+
+const profileContent = document.querySelector("#profile-content");
+console.log(profileContent);
+
+profileButton.addEventListener("click", toggleMenu);
+
+// on click we can toggle between display:block and display:none
+// by adding t
+function toggleMenu() {
+  profileContent.classList.toggle("show");
+}
+
+const bottomButton = document.querySelector("#bottom-button");
+console.log(bottomButton);
+bottomButton.addEventListener("click", gotoBottom);
+function gotoBottom() {
+  window.location.href = "#bottom";
+  //   window.location.href = "https://www.abc.net.au/"
+}
+
+const topButton = document.querySelector("#top-button");
+console.log(topButton);
+topButton.addEventListener("click", gotoTop);
+function gotoTop() {
+  window.location.href = "#top";
+}
+
+//Scroll stuff
+const scrollContent = document.querySelector("#scroll-content");
+
+const hscrollButton = document.querySelector("#hscroll-button");
+console.log(hscrollButton);
+
+hscrollButton.addEventListener("click", getHScroll);
+
+const hscroll = document.querySelector("#hscroll");
+
+function getHScroll() {
+  hscroll.textContent = scrollContent.scrollLeft;
+}
+
+const vscrollButton = document.querySelector("#vscroll-button");
+console.log(vscrollButton);
+
+vscrollButton.addEventListener("click", getVScroll);
+
+const vscroll = document.querySelector("#vscroll");
+
+function getVScroll() {
+  vscroll.textContent = scrollContent.scrollTop;
+}
+
+const hscroll150Button = document.querySelector("#hscroll150-button");
+console.log(hscroll150Button);
+
+hscroll150Button.addEventListener("click", gotoHScroll150);
+
+function gotoHScroll150() {
+  scrollContent.scrollTo({ left: 150, behavior: "smooth" });
+}
+
+const vscroll300Button = document.querySelector("#vscroll300-button");
+console.log(vscroll300Button);
+
+vscroll300Button.addEventListener("click", gotoVScroll300);
+
+function gotoVScroll300() {
+  scrollContent.scrollTo({ top: 300, behavior: "smooth" });
+}
+
+const wvscrollButton = document.querySelector("#wvscroll-button");
+console.log(wvscrollButton);
+
+wvscrollButton.addEventListener("click", getWVScroll);
+
+const wvscroll = document.querySelector("#wvscroll");
+
+function getWVScroll() {
+  wvscroll.textContent = window.scrollY;
+}
+
+//-----------------------------------------------------------
+
+// const hoverButton = document.querySelector("#hover-button");
+// console.log(hoverButton);
+
+// const moreInfo = document.querySelector("#more-info");
+// console.log(moreInfo);
+
+// hoverButton.addEventListener("mouseover", showMoreInfo);
+// hoverButton.addEventListener("mouseout", hideMoreInfo);
+
+// function showMoreInfo() {
+//   moreInfo.classList.add("show");
+// }
+
+// function hideMoreInfo() {
+//   moreInfo.classList.remove("show");
+// }
---
+++ week10/slides.js
@@ -0,0 +1,37 @@
+const slides = document.querySelectorAll(".slides");
+console.log(slides);
+
+const slidesContainer = document.querySelector("#slides-container");
+console.log(slidesContainer);
+
+let slideIndex = 0;
+let distance = 0;
+
+// add lcick events to navigate buttons
+const prevButton = document.querySelector("#prev-button");
+console.log(prevButton);
+prevButton.addEventListener("click", gotoPrevious);
+function gotoPrevious() {
+  if (slideIndex > 0) {
+    slideIndex--;
+  } else {
+    slideIndex = slides.length - 1;
+  }
+  distance = slides[slideIndex].offsetLeft;
+  console.log(distance);
+  window.scrollTo({ left: distance, behavior: "smooth" });
+}
+
+const nextButton = document.querySelector("#next-button");
+console.log(nextButton);
+nextButton.addEventListener("click", gotoNext);
+function gotoNext() {
+  if (slideIndex < slides.length - 1) {
+    slideIndex++;
+  } else {
+    slideIndex = 0;
+  }
+  distance = slides[slideIndex].offsetLeft;
+  console.log(distance);
+  window.scrollTo({ left: distance, behavior: "smooth" });
+}
